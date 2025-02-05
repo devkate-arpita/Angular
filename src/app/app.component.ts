7bf74036bc95cee581670af99d7bc77dd129cc42
ajinkya-sakharkar <asakharkar1998@gmail.com>
2025-02-05 18:15:14 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,10 +15,25 @@ export class AppComponent {
     this.isSidebarClosed = !this.isSidebarClosed;
   }
 
+  // toggleTheme() {
+  //   this.isDarkMode = !this.isDarkMode;
+  // }
+
   toggleTheme() {
-    this.isDarkMode = !this.isDarkMode;
+    this.isDarkMode = !this.isDarkMode;  // Toggle between light and dark mode
+
+    if (this.isDarkMode) {
+      // Set dark mode styles
+      document.body.style.backgroundColor = '#333333';  // Dark background
+      document.body.style.color = '#ffffff';  // Light text color
+    } else {
+      // Set light mode styles
+      document.body.style.backgroundColor = '#ffffff';  // Light background
+      document.body.style.color = '#000000';  // Dark text color
+    }
   }
 
+  
   toggleDropdown(topic: string) {
     this.activeDropdown = this.activeDropdown === topic ? null : topic;
   }
